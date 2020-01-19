e676fbd9b04072768c170889a92d3b080ba07dd9
ZarelaZanabria <zarelixzana@gmail.com>
2020-01-19 10:09:17 -0500

    Realiza las paginación de los elementos y navegación automatica de cada pagina en un tiempo de 5 seg

--- src/components/Panels/InProgressCleanings.vue
+++ src/components/Panels/InProgressCleanings.vue
@@ -17,7 +17,7 @@
                             <span @click="navigatePrev"> &#60; </span>
                             <span @click="navigateNext"> &#62; </span>
                         </div>
-                        <p>1/1</p>
+                        <p>{{page}}/{{numberPagination}}</p>
                     </div>
                 </div>
             </div>
@@ -25,20 +25,22 @@
         <div class="card-body p-3">
             <div class="row justify-content-center">
                 <div class="col-lg-4 col-md-6 col-xs-12 item-work">
-                    <div class="item">
+                    Hora de la Computadora {{hours}} - En segundos {{hoursInSeg}}
+                    <br>
+                    <div class="item" v-for="(item, index) in listDataxPage" :key="index">
                         <div class="media">
                             <div class="d-flex justify-content-center content-image">
                                 <img src="http://pluspng.com/img-png/download-happy-person-png-images-transparent-gallery-advertisement-275.png"
                                      width="75" height="75"/>
                             </div>
                             <div class="media-body">
-                                <h5 class="mt-0">Cristian Vega</h5>
-                                <p>Limpieza : <span>Normal</span></p>
-                                <p>Depa : <span>Adriana Pelagatti</span></p>
+                                <h5 class="mt-0">{{item.owner_department}}</h5>
+                                <p>Limpieza : <span>{{item.type_cleaning}}</span></p>
+                                <p>Depa : <span>{{item.user_staff}}</span></p>
                             </div>
                         </div>
                         <div>
-                            Tiempo de limpieza: 1:33:12
+                            Tiempo de limpieza: {{item.time}}
                         </div>
                     </div>
                 </div>
@@ -49,6 +51,8 @@
 
 <script>
 
+import jsonDataClean from '../../data/jsondata.json'
+
     export default {
         name: "inProgressPanel",
         components: {
@@ -56,13 +60,61 @@
         },
         data() {
             return {
-
+                dataCleanDepartment : jsonDataClean,
+                numberDataxPage : 6,
+                page : 1,
+                time : 0,
+                hours : '',
+                hoursInSeg : ''
             }
         },
         async mounted() {
-
+            setInterval(() => {
+                let time = new Date();
+                this.hours = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
+                this.hoursInSeg = time.getHours()*3600 + time.getMinutes()*60 + time.getSeconds();
+                }, 1000);
+                
         },
         computed: {
+            numberPagination () {    
+                 console.log(jsonDataClean.length, 'cantidad de data', jsonDataClean.length % this.numberDataxPage);             
+                if(jsonDataClean.length % this.numberDataxPage !==0){
+                    return  Math.floor(jsonDataClean.length / this.numberDataxPage)+1
+                }else {
+                    return  Math.floor(jsonDataClean.length / this.numberDataxPage)
+                }                
+               
+            },
+            listDataxPage(){
+                // eslint-disable-next-line vue/no-async-in-computed-properties
+                setTimeout(() => {
+                    this.navigateNext();
+                }, 5000); 
+            
+                let dataListar = this.dataCleanDepartment.filter ((element, index)=>{                
+                                                  
+                if ((this.page-1)* this.numberDataxPage <= index && this.page * this.numberDataxPage > index) {
+                    
+                        const partHorasObject = element.time.split(':');
+                        console.log('array Separdo', partHorasObject);
+                        const hoursInSegundos = parseInt(partHorasObject[0].trim())*3600;
+                        const minutosInSegundos = parseInt(partHorasObject[1].trim())*60;
+                        const segundos = parseInt(partHorasObject[2].trim())
+                        const horaObjectInSegunds = hoursInSegundos + minutosInSegundos + segundos;
+                        console.log('TOTAL ', horaObjectInSegunds  );
+                        
+                        console.log('Hora en Segundos', hoursInSegundos, 'MINUTOS', minutosInSegundos, 'SEGUNDOS', segundos);
+                        
+                        
+                       console.log('ELEMENTO' , element.time);        
+                        return this.dataCleanDepartment[index];                            
+                    }
+                })
+                
+                return dataListar;
+            }
+
 
         },
         methods: {
@@ -73,11 +125,22 @@
                 this.$store.dispatch('home/progressCleaning', this.$parent.auth);
             },
             navigatePrev() {
+               this.page == 1 ? this.page = 1 : this.page -=1;
 
             },
             navigateNext() {
+              this.numberPagination == this.page ? this.page = 1 : this.page +=1;
+            },
+            convertirHoraASegundos (){
+            
+            },
+
+            convertirSegundosAHora(){
 
             }
+
+        
+            
         }
     }
 </script>
